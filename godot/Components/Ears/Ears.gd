12eb59159b697b6ac0e3c0ab713304daceb24bb0
AshladBP <imadbenbp@gmail.com>
2024-05-12 20:16:49 +0200

    Adding earing features

---
+++ godot/Components/Ears/Ears.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+@export var radius: float = 50.0
+@onready var character = get_parent()
+
+func _ready():
+	var area = Area2D.new()
+	var collisionShape = CollisionShape2D.new() 
+	area.add_child(collisionShape)
+	var circleShape = CircleShape2D.new()
+	circleShape.radius = radius
+	collisionShape.shape = circleShape
+	add_child(area)
+	area.connect("body_entered",_on_body_entered)
+
+func _on_body_entered(body: Node):
+	if body == character:
+		return
+	print("Body entered:", body)
